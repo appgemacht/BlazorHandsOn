e65efd42bc36241ed791f8899d36de779a0d05ed
Jean-Marc Vogel <4463775+appgemacht@users.noreply.github.com>
2022-08-25 15:31:26 +0200

    Mark ToDos as completed.

---
+++ BlazorTodoApp/BlazorTodoApp.Web/Models/ToDo.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorTodoApp.Web.Models
+{
+    public class ToDo
+    {
+        public ToDo(string text)
+        {
+            Text = text;
+        }
+
+        public string Text { get; }
+
+        public bool IsComplete { get; set; }
+    }
+}
